8bb980edffe59887d994137733f5d2d47fb2d218
Wim Muskee <wimmuskee@gmail.com>
2016-09-13 06:35:51 +0200

    and one more for exactMatch

--- init/table_senseReference.sql
+++ init/table_senseReference.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS senseReference;
 CREATE TABLE senseReference (
 lexicalSenseID INT UNSIGNED NOT NULL,
 namespace ENUM('ontolex','skos') NOT NULL,
-property ENUM('reference','evokes','narrower') NOT NULL,
+property ENUM('reference','evokes','narrower','exactMatch') NOT NULL,
 reference VARCHAR(150) NOT NULL
 );
