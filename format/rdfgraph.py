a42d29ee8fcbede6a320ceb55f18c524d8234690
Wim Muskee <wimmuskee@gmail.com>
2016-09-17 17:51:23 +0200

    export transitives, based on input config sense ids

--- format/rdfgraph.py
+++ format/rdfgraph.py
@@ -46,6 +46,13 @@ class RDFGraph():
 			self.g.add((s,SKOS.broader,o))
 
 
+	def setTransitives(self,senseIdentifiers):
+		""" This function is dependent on setInverses and setRedundants """
+		for senseIdentifier in senseIdentifiers:
+			for narrowerID in self.g.objects(URIRef(senseIdentifier),SKOS.narrower):
+				self.__setTransitive(narrowerID,senseIdentifier)
+
+
 	def printGraph(self):
 		if self.buildpackage:
 			import datetime
@@ -53,3 +60,11 @@ class RDFGraph():
 			self.g.add((URIRef("urn:" + self.name),VOID.triples,Literal(str(len(self.g)+1), datatype=XSD.integer)))
 
 		print(bytes.decode(self.g.serialize(format=self.format)))
+
+
+	def __setTransitive(self,startSenseID,targetSenseID):
+		""" Move down the graph, and store transitives (also inverses), and move down again."""
+		for narrowerID in self.g.objects(URIRef(startSenseID),SKOS.narrower):
+			self.g.add((URIRef(narrowerID),SKOS.broaderTransitive,URIRef(targetSenseID)))
+			self.g.add((URIRef(targetSenseID),SKOS.narrowerTransitive,URIRef(narrowerID)))
+			self.__setTransitive(narrowerID,targetSenseID)
